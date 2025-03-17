decaffb45b1d25090aefa53ab37342242e094a8a
Abdulmunaim Mahfudh <113377393+Abdulmunimsaleh@users.noreply.github.com>
2025-03-17 18:12:22 +0300

    start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Install Playwright browsers
-python -m playwright install chromium
+python -m playwright install chromium --with-deps
 # Start the FastAPI app
 uvicorn main:app --host 0.0.0.0 --port $PORT
